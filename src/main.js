0ab56af328ce78301ff6bc90319952b37146c71c
Oziel Sousa <ozielzin358@gmail.com>
2024-08-23 14:38:10 -0300

    fix: file structure

--- src/main.js
+++ src/main.js
@@ -1,53 +1,53 @@
-import {APIKEY} from "./api_key.js"
+import { APIKEY } from "./api_key.js";
 const textInput = document.getElementById("textInput");
 const btnInput = document.getElementsByClassName("btn")[0];
 const resultText = document.getElementById("resultText");
 
-const query = async (data)=>{
-  const response = await fetch("https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment", 
+const query = async (data) => {
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment",
     {
       headers: {
-        Authorization: `Bearer ${APIKEY}`
+        Authorization: `Bearer ${APIKEY}`,
       },
       method: "POST",
       body: JSON.stringify(data),
     }
   );
-  const result = await response.json()
-  return result[0]
-}
+  const result = await response.json();
+  return result[0];
+};
 
-const convertLabelToString = (label)=>{
+const convertLabelToString = (label) => {
   switch (label) {
     case "5 stars":
-      return "Muito Positivo"
+      return "Muito Positivo";
     case "4 stars":
-      return "Positivo"
+      return "Positivo";
     case "3 stars":
-      return "Neutro"
+      return "Neutro";
     case "2 stars":
-      return "Negativo"
+      return "Negativo";
     case "1 star":
-      return "Muito Negativo"
+      return "Muito Negativo";
     default:
-      return "Não foi possivel detectar o sentimento!"
+      return "Não foi possivel detectar o sentimento!";
   }
-}
+};
 
-btnInput.addEventListener("click", ()=>{
+btnInput.addEventListener("click", () => {
   if (textInput.value.trim() === "") {
-    return alert("Digite algum texto!")
+    return alert("Digite algum texto!");
   }
   query({ inputs: `${textInput.value}` }).then((response) => {
- 
-    const resultStrings = response.map(item => {
-    const sentimentText = convertLabelToString(item.label) 
-    const percentage = (item.score * 100).toFixed(2)
-    return `${percentage} % ${sentimentText}`
-    })
+    const resultStrings = response.map((item) => {
+      const sentimentText = convertLabelToString(item.label);
+      const percentage = (item.score * 100).toFixed(2);
+      return `${percentage} % ${sentimentText}`;
+    });
 
-    resultText.innerHTML = `O Sentimento dessa frase é <span style="font-weight: bold;"><br>${resultStrings.join("<br>")}</span>`;  
+    resultText.innerHTML = `O Sentimento dessa frase é <span style="font-weight: bold;"><br>${resultStrings.join(
+      "<br>"
+    )}</span>`;
+  });
 });
-})
-
-
